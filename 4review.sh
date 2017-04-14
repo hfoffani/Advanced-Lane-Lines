b805bfdf5218c03819a6d78cca9fbd7d1d1db357
hfoffani <hfoffani@gmail.com>
2017-04-14 17:58:25 +0200

    reviewer tool

--- 4review.sh
+++ 4review.sh
@@ -1,2 +1,10 @@
-FILES="writeup_report.pdf advanced_lane.mp4 image-proc.ipynb output_images/*.png"
+MUSTBE="README.pdf advanced_lane.mp4 image-proc.ipynb"
+FILES="$MUSTBE output_images/*.png"
+
+if [ `ls -1 $MUSTBE 2>/dev/null | wc -l` -ne 3 ]
+then
+    echo "missing file(s)"
+    exit 1
+fi
 zip project4.zip $FILES
+
